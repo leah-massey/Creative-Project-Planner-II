65b37e68eedf40b24b062754e626de1ba28934d6
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-19 20:24:45 +0100

    Create new project established through POST request.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const express = require("express");
 
 const app = express();
+app.use(express.json()); // express.json() is the middleware - post request doesn't work without this!
 
 const projects = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/projects.json`)
@@ -11,6 +12,11 @@ app.get("/api/v1/projects", (req, res) => {
   res
     .status(200)
     .json({ status: "success", results: projects.length, data: { projects } });
+});
+
+app.post("/api/v1/projects", (req, res) => {
+  console.log(req.body);
+  res.send("done"); // we always need to send something back
 });
 
 const port = 3002;
