6b8b1cc67e2b294dca899c9a76e07435eace16c4
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-24 22:04:40 +0100

    .deleteProject method now connected to database.

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -98,9 +98,18 @@ exports.updateProject = async (req, res) => {
   }
 };
 
-exports.deleteProject = (req, res) => {
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
+exports.deleteProject = async (req, res) => {
+  try {
+    await Project.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
