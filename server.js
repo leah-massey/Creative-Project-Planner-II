cf241ed1fc3f24e3cd061bce085f2e7bb83cd51b
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-24 16:18:30 +0100

    Project schema and model created.

--- server.js
+++ server.js
@@ -16,6 +16,27 @@ mongoose
   })
   .then(() => console.log("DB connection successful!"));
 
+const projectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A project must have a name"],
+    unique: true,
+  },
+  category: {
+    type: String,
+    required: [true, "A project must have a category"],
+  },
+  description: {
+    type: String,
+    default: "You currently don't have any further notes on this project.",
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Project = mongoose.model("Project", projectSchema);
 // console.log(process.env);
 
 const port = process.env.PORT || 3002;
